f8a29f181d6837e0401d379f5d04aa3993db62ff
Joshua Lawrence <xusies@live.com>
2024-08-12 12:30:48 +0930

    changeg core commands cost from int to string, defined typeName for Deployment core abilities

--- js/ui.js
+++ js/ui.js
@@ -18,7 +18,7 @@ let coreAbilities = [
     {
         chars:{
             Declare:"Pick a friendly unit that is not in combat to use this ability.",
-            Cost:1,
+            Cost:"1",
             Effect:"Make 6 rally rolls of D6. For each 4+, you receive 1 rally point. Rally points can be spent in the following ways: For each rally point spent, **Heal (1)** that unit. You can spend a number of rally points equal to the Health characteristic of that unit to return a slain model to that unit. You can spend the rally points in any combination of the above. Unspent rally points are then lost.",
             Keywords:"",
             Timing:"Any Hero Phase",
@@ -29,7 +29,7 @@ let coreAbilities = [
     {
         chars:{
             Declare:"Pick a friendly Wizard or Priest to use this ability.",
-            Cost:1,
+            Cost:"1",
             Effect:"That friendly unit can use a **Spell** or **Prayer ability** (as appropriate) as if it were your hero phase. If you do so, subtract 1 from **casting rolls** or **chanting rolls** made as part of that ability.",
             Keywords:"",
             Timing:"Enemy Hero Phase",
@@ -40,7 +40,7 @@ let coreAbilities = [
     {
         chars:{
             Declare:"Pick a friendly unit that is not in combat to use this ability",
-            Cost:1,
+            Cost:"1",
             Effect:"Each model in that unit can move up to D6\" at move **cannot** pass through or end within the combat range of an enemy unit.",
             Keywords:"**^^Move^^**, **^^Run^^**",
             Timing:"",
@@ -51,7 +51,7 @@ let coreAbilities = [
     {
         chars:{
             UsedBy:"The unit using that **^^Run^^** ability",
-            Cost:1,
+            Cost:"1",
             Effect:"Do not make a **run roll** as part of that Run ability. Instead, add 6\" to that unit's **^^Move^^** characteristic to determine the distance each model in that unit can move as part of that **^^Run^^** ability.",
             Keywords:"",
             Timing:"Reaction: You declared a **^^Run^^**",
@@ -62,7 +62,7 @@ let coreAbilities = [
     {
         chars:{
             Declare:"Pick a friendly unit that is **not in combat** to use this ability.",
-            Cost:1,
+            Cost:"1",
             Effect:"Resolve **shooting attacks** for that unit, but all of the attacks must target the **nearest visible enemy** unit and you must subtract 1 from the **hit rolls** for those attacks.",
             Keywords:"",
             Timing:"Enemy Shooting Phase",
@@ -73,7 +73,7 @@ let coreAbilities = [
     {
         chars:{
             Declare:"Pick a friendly unit that is **not in combat** to use this ability.",
-            Cost:2,
+            Cost:"2",
             Effect:"That unit can use a **^^Charge^^** ability as if it were your charge phase.",
             Keywords:"",
             Timing:"",
@@ -84,7 +84,7 @@ let coreAbilities = [
     {
         chars:{
             UsedBy:"The unit using that **^^Charge^^** ability.",
-            Cost:1,
+            Cost:"1",
             Effect:"You can re-roll the **charge roll**.",
             Keywords:"",
             Timing:"Reaction: You declared a **^^Charge^^** ability.",
@@ -95,7 +95,7 @@ let coreAbilities = [
     {
         chars:{
             UsedBy:"The unit using that **^^Attack^^** ability",
-            Cost:1,
+            Cost:"1",
             Effect:"Add 1 to **hit rolls** for attacks made as part of that **^^Attack^^**  ability. This also affects weapons that have the **Companion** weapon ability.",
             Keywords:"",
             Timing:"Reaction: You declared an **^^Attack^^** ability.",
@@ -106,7 +106,7 @@ let coreAbilities = [
     {
         chars:{
             UsedBy:"A unit targeted by that **^^Attack^^** ability",
-            Cost:1,
+            Cost:"1",
             Effect:"Add 1 to **save rolls** for that unit in this phase.",
             Keywords:"",
             Timing:"Reaction: Opponent declared an **^^Attack^^** ability.",
@@ -117,7 +117,7 @@ let coreAbilities = [
     {
         chars:{
             Declare:"Pick a friendly unit that charged this turn to use this ability, then you must pick an enemy unit in combat with it to be the target. The target must have a lower **Health** characteristic than the unit using this ability.",
-            Cost:1,
+            Cost:"1",
             Effect:": Inflict **D3 mortal damage** on the target. Then, the unit using this ability can move a distance up to its **Move** characteristic. It can pass through and end that move within the combat ranges of enemy units that were in combat with it at the start of the move, but not those of other enemy units. It does not have to end the move in combat",
             Keywords:"**^^Move^^**",
             Timing:"End of Any Turn",
@@ -193,7 +193,7 @@ let coreAbilities = [
             Timing:"Deployment Phase",
         },
         name: "Deploy Unit",
-        typeName: ""
+        typeName: "Ability (Activated)"
     },
     {
         chars:{
@@ -203,7 +203,7 @@ let coreAbilities = [
             Timing:"Deployment Phase",
         },
         name: "Deploy Faction Terrain",
-        typeName: ""
+        typeName: "Ability (Activated)"
     },
     {
         chars:{
@@ -213,7 +213,7 @@ let coreAbilities = [
             Timing:"Deployment Phase",
         },
         name: "Deploy Regiment",
-        typeName: ""
+        typeName: "Ability (Activated)"
     },
 ];
 
@@ -681,7 +681,7 @@ function createAbilityDiv(ability){
     let unTitle = document.createElement("label");
     unTitle.innerHTML = "Units";
     unitsDiv.appendChild(unTitle);
-    ability.units.forEach(unitIdx=>{
+    ability.units?.forEach(unitIdx=>{
         let unit = null;
         if(typeof(unitIdx) == "string" && unitIdx?.slice(0,1) == 'r'){
             let ror_idx = unitIdx.slice(1).split('_')[0];
@@ -699,6 +699,7 @@ function createAbilityDiv(ability){
 }
 
 function createAbilityCharDiv(char,val){
+    console.log(char,val)
     let div = document.createElement("div")
     let span = document.createElement("span");
     let label = document.createElement("label");
